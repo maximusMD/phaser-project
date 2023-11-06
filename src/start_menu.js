3193e6329ed6781fb93e8f8be3833ae712538096
maximusMD <maximilian.dunne@gmail.com>
2023-11-06 12:57:31 +0000

    This commit will add the structure of the start menu

--- src/start_menu.js
+++ src/start_menu.js
@@ -1,4 +1,7 @@
 import Phaser from 'phaser'
+import guestImg from './assets/guest.png'
+import lbImg from './assets/leaderboard.png'
+import signInImg from './assets/sign-in.png'
 
 export class MenuScene extends Phaser.Scene {
     constructor () {
@@ -7,16 +10,24 @@ export class MenuScene extends Phaser.Scene {
 
     preload () {
         this.load.image('background', 'assets/bg.png')
+        this.load.image('title', 'assets/title.png')
+        this.load.image('sign-in', signInImg)
+        this.load.image('guest', guestImg)
+        this.load.image('leaderboard', lbImg)
     }
 
     create () {
-        const background = this.add.image(0, 0, 'background');
-
         const gameWidth = this.cameras.main.width;
         const gameHeight = this.cameras.main.height;
-    
+        
+        const background = this.add.image(0, 0, 'background');    
         background.displayWidth = gameWidth;
         background.displayHeight = gameHeight;
         background.setPosition(gameWidth / 2, gameHeight / 2)
+
+        const title = this.add.image(0, 0, 'title')
+        const signIn = this.add.image(420, 310, 'sign-in')
+        const guest = this.add.image(420, 380, 'guest')
+        const lb = this.add.image(420, 525, 'leaderboard')
     }
 }
