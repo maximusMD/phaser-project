5d5478bd3a948d4cdd6b058f4411bd2d4ff3ba7d
Domaeos <ryanmurphy.ic@icloud.com>
2023-11-07 09:07:32 +0000

    Window resize, base classes and test level

---
+++ src/Actor.js
@@ -0,0 +1,27 @@
+import { Physics } from 'phaser';
+
+export class Actor extends Physics.Arcade.Sprite {
+    #hp = 100;
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y,);
+        this.setTexture(texture);
+        this.setFrame(frame);
+        scene.add.existing(this)
+        scene.physics.add.existing(this)
+
+        this.setCollideWorldBounds(true);
+    }
+    getHP() {
+        return this.#hp;
+    }
+    checkFlip() {
+        if (this.body.velocity.x < 0) {
+            this.scaleX = -1;
+        } else {
+            this.scaleX = 1;
+        }
+    }
+    getBody() {
+        return this.body;
+    }
+}       
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import Phaser from 'phaser';
 import { MenuScene } from './start_menu';
+import { RyanLevel } from './Ryan_test_level';
+
+window.sizeChanged = () => {
+    if (game.isBooted) {
+        setTimeout(() => {
+            game.scale.resize(window.innerWidth, window.innerHeight);
+            game.canvas.setAttribute(
+                'style',
+                `display: block; width: ${window.innerWidth}px; height: ${window.innerHeight}px;`,
+            );
+        }, 100);
+    }
+};
+window.onresize = () => window.sizeChanged();
+
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
     scene: MenuScene
 }
 
 const game = new Phaser.Game (config)
+
