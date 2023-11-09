c6c43a6a9adf06468a161cf4c68b485f09d6d0fc
Domaeos <ryanmurphy.ic@icloud.com>
2023-11-09 22:41:31 +0000

    added function for enabling/disabling weather

--- src/classes/Weather.js
+++ src/classes/Weather.js
@@ -1,6 +1,7 @@
 import rain from "../assets/animations/sprites/rain.png"
 
 export class Weather {
+    #enabled = true;
     #rainSpeed;
     // high number = stronger wind
     #windSpeed;
@@ -23,7 +24,16 @@ export class Weather {
     getRainSpeed() {
         return this.#rainSpeed;
     }
-
+    getEnabled() {
+        return this.#enabled;
+    }
+    disable() {
+        this.removeRain();
+        this.#enabled = false;
+    }
+    enable() {
+        this.#enabled = true;
+    }
     setWindSpeed(speed) {
         if (typeof speed === "number") {
             this.#windSpeed = speed;
@@ -44,31 +54,37 @@ export class Weather {
     }
 
     addRain() {
-        this.scene.rain_emitter = this.scene.add.particles(-10, -150, 'rain', {
-            scale: 0.1,
-            gravityY: 300,
-            gravityX: this.getWindSpeed(),
-            lifespan: 5000,
-            frequency: this.getRainFrequency(),
-            maxVelocityX: this.getRainSpeed(),
-            maxVelocityY: this.getRainSpeed(),
-        })
-        if (!this.scene.rain_zone) {
-            this.scene.rain_zone = new Phaser.Geom.Rectangle(0, 0, this.scene.scale.width, 10)
-            this.scene.rain_zone.centerX = this.scene.player.getBody().x;
+        if (this.#enabled) {
+            this.scene.rain_emitter = this.scene.add.particles(-10, -150, 'rain', {
+                scale: 0.1,
+                gravityY: 300,
+                gravityX: this.getWindSpeed(),
+                lifespan: 5000,
+                frequency: this.getRainFrequency(),
+                maxVelocityX: this.getRainSpeed(),
+                maxVelocityY: this.getRainSpeed(),
+            })
+            if (!this.scene.rain_zone) {
+                this.scene.rain_zone = new Phaser.Geom.Rectangle(0, 0, this.scene.scale.width, 10)
+                this.scene.rain_zone.centerX = this.scene.player.getBody().x;
+            }
+
+            this.scene.rain_emitter.addEmitZone({ type: 'random', source: this.scene.rain_zone })
+
         }
-
-        this.scene.rain_emitter.addEmitZone({ type: 'random', source: this.scene.rain_zone })
-
     }
 
     removeRain() {
-        this.scene.rain_emitter.destroy();
+        if (this.scene.rain_emitter) {
+            this.scene.rain_emitter?.destroy();
+        }
     }
 
     update() {
-        if (this.scene.rain_zone) {
-            this.scene.rain_zone.centerX = this.scene.player.getBody().x;
+        if (this.#enabled = true) {
+            if (this.scene.rain_zone) {
+                this.scene.rain_zone.centerX = this.scene.player.getBody().x;
+            }
         }
     }
 }
