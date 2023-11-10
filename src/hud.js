ba11aaa60935c57bc429a427d776001e9f0ff97b
maximusMD <maximilian.dunne@gmail.com>
2023-11-10 12:21:55 +0000

    Health now implemented and animated within HUD, frame for logic exists but not yet implemented

--- src/hud.js
+++ src/hud.js
@@ -1,5 +1,8 @@
 import Phaser from 'phaser'
 
+import heart_image from './assets/animations/sprites/Heart GUI/heart_atlas.png'
+import heart_atlas from './assets/animations/sprites/Heart GUI/heart_atlas.json'
+
 export class HUDScene extends Phaser.Scene {
 
     constructor () {
@@ -12,15 +15,20 @@ export class HUDScene extends Phaser.Scene {
             'webfont',
             'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
         )
+
+        this.load.atlas('health', heart_image, heart_atlas)
     }
 
     create() {
+        const gameWidth = this.cameras.main.width;
+        const gameHeight = this.cameras.main.height;
+
         WebFont.load({
             google: {
                 families: ['Pixelify Sans'],
             },
             active: () => {
-                let info = this.add.text(10, 10, 'Score: 0', {
+                let info = this.add.text(gameWidth * 0.05, gameHeight * 0.05, 'Score: 0', {
                     fontFamily: 'Pixelify Sans',
                     fontSize: '48px',
                     fill: '#FFFFFF',
@@ -32,5 +40,32 @@ export class HUDScene extends Phaser.Scene {
                 }, this)
             }
         })
+
+        const healthBar = this.add.sprite(gameWidth * 0.1123, gameHeight * 0.15, 'health', 'Heart-0.png');
+        const healthBarScaleFactor = gameWidth / healthBar.width
+        healthBar.setScale(healthBarScaleFactor * 0.135)
+    
+        this.anims.create({
+            key: 'healthAnimation',
+            frames: this.anims.generateFrameNames('health', {
+                prefix: 'Heart-',
+                start: 0,
+                end: 4,
+                zeroPad: 1,
+                suffix: '.png',
+            }),
+            repeat: -1,
+            frameRate: 3
+        });
+    
+        healthBar.play('healthAnimation');
     }
+
+    // updateHealthBar() {
+    //     const healthPercentage = // Calculate health percentage;
+    //     const frameIndex = Math.floor(healthPercentage * 4);
+
+    //     Set the frame based on the health percentage
+    //     healthBar.setFrame(`Heart-${frameIndex}.png`);
+    // }
 }
