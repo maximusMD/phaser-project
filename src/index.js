67ea3c34d26981d9753a6193b098d869ccc692c6
sweepas <47362063+sweepas@users.noreply.github.com>
2023-11-10 15:26:45 +0000

    leaderboard scene

--- src/index.js
+++ src/index.js
@@ -1,37 +1,47 @@
-import Phaser from 'phaser';
-import { Preloader } from './Preloader';
-import { MenuScene } from './start_menu';
-import { OptionsScene } from './options_menu';
-import { UserForm } from './UserForm';
-import { RyanLevel } from './Ryan_test_level';
-import { CreditsScene } from './credits';
-import { ControlsScene } from './controls';
-import { AboutScene } from './about';
-import { HUDScene } from './hud';
-
+import Phaser from "phaser";
+import { Preloader } from "./Preloader";
+import { MenuScene } from "./start_menu";
+import { OptionsScene } from "./options_menu";
+import { UserForm } from "./UserForm";
+import { RyanLevel } from "./Ryan_test_level";
+import { CreditsScene } from "./credits";
+import { ControlsScene } from "./controls";
+import { AboutScene } from "./about";
+import { HUDScene } from "./hud";
+import { LeaderboardScene } from "./leaderboard";
 
 window.sizeChanged = () => {
-    if (game.isBooted) {
-        setTimeout(() => {
-            game.scale.resize(window.innerWidth, window.innerHeight);
-            game.canvas.setAttribute(
-                'style',
-                `display: block; width: ${window.innerWidth}px; height: ${window.innerHeight}px;`,
-            );
-        }, 100);
-    }
+  if (game.isBooted) {
+    setTimeout(() => {
+      game.scale.resize(window.innerWidth, window.innerHeight);
+      game.canvas.setAttribute(
+        "style",
+        `display: block; width: ${window.innerWidth}px; height: ${window.innerHeight}px;`
+      );
+    }, 100);
+  }
 };
 window.onresize = () => window.sizeChanged();
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: window.innerWidth,
-    height: window.innerHeight,
-    scene: [Preloader, MenuScene, OptionsScene, ControlsScene, UserForm, RyanLevel, CreditsScene, AboutScene, HUDScene],
-    render: { pixelArt: true},
-    dom: { createContainer: true}, 
-}
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: window.innerWidth,
+  height: window.innerHeight,
+  scene: [
+    Preloader,
+    MenuScene,
+    OptionsScene,
+    ControlsScene,
+    UserForm,
+    RyanLevel,
+    CreditsScene,
+    AboutScene,
+    HUDScene,
+    LeaderboardScene,
+  ],
+  render: { pixelArt: true },
+  dom: { createContainer: true },
+};
 
-const game = new Phaser.Game(config)
-
+const game = new Phaser.Game(config);
--- src/start_menu.js
+++ src/start_menu.js
@@ -1,97 +1,132 @@
-import Phaser from 'phaser';
-import guestImg from './assets/menu_buttons/guest.png';
-import lbImg from './assets/menu_buttons/leaderboard.png';
-import signInImg from './assets/menu_buttons/sign-in-up.png';
-import shareImg from './assets/menu_buttons/share.png';
-import credImg from './assets/menu_buttons/credits.png';
-import optImg from './assets/menu_buttons/options.png';
-import aboutImg from './assets/menu_buttons/about.png';
-import menuMusic from './assets/menuMusic.wav';
+import Phaser from "phaser";
+import guestImg from "./assets/menu_buttons/guest.png";
+import lbImg from "./assets/menu_buttons/leaderboard.png";
+import signInImg from "./assets/menu_buttons/sign-in-up.png";
+import shareImg from "./assets/menu_buttons/share.png";
+import credImg from "./assets/menu_buttons/credits.png";
+import optImg from "./assets/menu_buttons/options.png";
+import aboutImg from "./assets/menu_buttons/about.png";
+import menuMusic from "./assets/menuMusic.wav";
 
 export class MenuScene extends Phaser.Scene {
-    constructor() {
-        super({ 
-            key: 'MenuScene'
-        });
+  constructor() {
+    super({
+      key: "MenuScene",
+    });
+  }
+
+  preload() {
+    this.load.image("background", "assets/bg.png");
+    this.load.image("title", "assets/title.png");
+    this.load.image("sign-in", signInImg);
+    this.load.image("guest", guestImg);
+    this.load.image("leaderboard", lbImg);
+    this.load.image("credits", credImg);
+    this.load.image("share", shareImg);
+    this.load.image("options", optImg);
+    this.load.image("about", aboutImg);
+
+    this.load.audio("menuMusic", menuMusic);
+  }
+
+  create() {
+    const gameWidth = this.cameras.main.width;
+    const gameHeight = this.cameras.main.height;
+
+    const background = this.add.image(0, 0, "background");
+    background.displayWidth = gameWidth;
+    background.displayHeight = gameHeight;
+    background.setPosition(gameWidth / 2, gameHeight / 2);
+
+    const title = this.add.image(gameWidth * 0.52, gameHeight * 0.29, "title");
+    const titleScaleFactor = gameWidth / title.width;
+    title.setScale(titleScaleFactor * 0.55);
+
+    const signIn = this.addButton(
+      gameWidth * 0.517,
+      gameHeight * 0.48,
+      "sign-in",
+      () => {
+        this.stopMenuMusicAndStartScene("UserForm");
+      }
+    );
+
+    const guest = this.addButton(
+      gameWidth * 0.517,
+      gameHeight * 0.63,
+      "guest",
+      () => {
+        this.stopMenuMusicAndStartScene("RyanLevel");
+      }
+    );
+
+    const lb = this.addButton(
+      gameWidth * 0.517,
+      gameHeight * 0.855,
+      "leaderboard",
+      () => {
+        this.stopMenuMusicAndStartScene("LeaderboardScene");
+      }
+    );
+
+    const about = this.addButton(
+      gameWidth * 0.1,
+      gameHeight * 0.1,
+      "about",
+      () => {
+        this.stopMenuMusicAndStartScene("AboutScene");
+      }
+    );
+
+    const credits = this.addButton(
+      gameWidth * 0.1,
+      gameHeight * 0.9,
+      "credits",
+      () => {
+        this.stopMenuMusicAndStartScene("CreditsScene");
+      }
+    );
+
+    const share = this.addButton(
+      gameWidth * 0.915,
+      gameHeight * 0.9,
+      "share",
+      () => {
+        console.log("Share");
+      }
+    );
+
+    const options = this.addButton(
+      gameWidth * 0.9,
+      gameHeight * 0.1,
+      "options",
+      () => {
+        this.stopMenuMusicAndStartScene("OptionsScene");
+      }
+    );
+
+    const menuMusic = this.sound.add("menuMusic");
+    if (!menuMusic.isPlaying) {
+      menuMusic.play();
     }
+  }
 
-    preload() {
-        this.load.image('background', 'assets/bg.png');
-        this.load.image('title', 'assets/title.png');
-        this.load.image('sign-in', signInImg);
-        this.load.image('guest', guestImg);
-        this.load.image('leaderboard', lbImg);
-        this.load.image('credits', credImg);
-        this.load.image('share', shareImg);
-        this.load.image('options', optImg);
-        this.load.image('about', aboutImg);
+  addButton(x, y, key, onClick) {
+    const button = this.add.image(x, y, key);
+    button.setInteractive();
+    button.on("pointerdown", onClick);
+    return button;
+  }
 
-        this.load.audio('menuMusic', menuMusic);
+  stopMenuMusicAndStartScene(sceneKey) {
+    const menuMusic = this.sound.get("menuMusic");
+    if (menuMusic) {
+      menuMusic.stop();
     }
+    this.scene.start(sceneKey);
+  }
 
-    create() {
-        const gameWidth = this.cameras.main.width;
-        const gameHeight = this.cameras.main.height;
-
-        const background = this.add.image(0, 0, 'background');
-        background.displayWidth = gameWidth;
-        background.displayHeight = gameHeight;
-        background.setPosition(gameWidth / 2, gameHeight / 2);
-
-        const title = this.add.image(gameWidth * 0.52, gameHeight * 0.29, 'title');
-        const titleScaleFactor = gameWidth / title.width;
-        title.setScale(titleScaleFactor * 0.55);
-
-        const signIn = this.addButton(gameWidth * 0.517, gameHeight * 0.48, 'sign-in', () => {
-            this.stopMenuMusicAndStartScene('UserForm');
-        });
-
-        const guest = this.addButton(gameWidth * 0.517, gameHeight * 0.63, 'guest', () => {
-            this.stopMenuMusicAndStartScene('RyanLevel');
-        });
-
-        const lb = this.addButton(gameWidth * 0.517, gameHeight * 0.855, 'leaderboard', () => {
-            console.log('Leaderboard');
-        });
-
-        const about = this.addButton(gameWidth * 0.1, gameHeight * 0.1, 'about', () => {
-            this.stopMenuMusicAndStartScene('AboutScene');
-        });
-
-        const credits = this.addButton(gameWidth * 0.1, gameHeight * 0.9, 'credits', () => {
-            this.stopMenuMusicAndStartScene('CreditsScene');
-        });
-
-        const share = this.addButton(gameWidth * 0.915, gameHeight * 0.9, 'share', () => {
-            console.log('Share');
-        });
-
-        const options = this.addButton(gameWidth * 0.9, gameHeight * 0.1, 'options', () => {
-            this.stopMenuMusicAndStartScene('OptionsScene');
-        });
-
-        const menuMusic = this.sound.add('menuMusic');
-        if (!menuMusic.isPlaying) {
-            menuMusic.play();
-        }
-    }
-
-    addButton(x, y, key, onClick) {
-        const button = this.add.image(x, y, key);
-        button.setInteractive();
-        button.on('pointerdown', onClick);
-        return button;
-    }
-
-    stopMenuMusicAndStartScene(sceneKey) {
-        const menuMusic = this.sound.get('menuMusic');
-        if (menuMusic) {
-            menuMusic.stop();
-        }
-        this.scene.start(sceneKey);
-    }
-
-    // openExternalLink(url) {
-    //     window.open(url, '_blank');
-    // }
+  // openExternalLink(url) {
+  //     window.open(url, '_blank');
+  // }
 }
