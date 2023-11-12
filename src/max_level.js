2f41c60afa033412f38b43dda556c606afbc8509
maximusMD <maximilian.dunne@gmail.com>
2023-11-12 21:21:56 +0000

    object hell, just pushing so its at its most recent for everyone tomoz, and in case someone can figure out how to get it working before i do

--- src/max_level.js
+++ src/max_level.js
@@ -60,7 +60,7 @@ export class MaxLevel extends Phaser.Scene {
         this.load.image('laser', laser_img)
         this.load.image('dust', dust)
 
-        this.load.image('metroid hc', tileset_img);
+        this.load.image('metroid hc', tileset_img, { frameWidth: 16, frameHeight: 16 });
         this.load.tilemapTiledJSON('tilemap', tilemap);
         this.cameras.main.setZoom(2, 2);
         this.load.atlas("rogue_player", rogue_image, rogue_atlas)
@@ -78,9 +78,34 @@ export class MaxLevel extends Phaser.Scene {
         this.bg3 = map.createLayer('bg3', tileset);
         this.background = map.createLayer('Background', tileset);
         this.bg4 = map.createLayer('b4', tileset);
-
         
         this.ground.setCollisionByExclusion(-1, true)
+
+        map.createFromObjects('PITS')
+
+        // objectLayer.objects.forEach((object) => {
+        // const gid = object.gid;
+
+        // const imageKey = map.tilesets[0].getTileProperties(gid);
+
+        // const sprite = this.add.sprite(object.x, object.y, imageKey);
+        // sprite.setOrigin(0, 1);
+        // });
+
+//         const objectLayer = map.getObjectLayer('PITS')
+// // Iterate through the objects in the object layer
+// objectLayer.objects.forEach((object) => {
+//    const tileId = object.gid & 0x1FFFFFFF;
+
+//    const tileProperties = map.tilesets[3].getTileProperties(tileId);
+
+//    if (tileProperties) {
+//        const sprite = this.add.sprite(object.x, object.y, tileset.image, tileId);
+//        sprite.setOrigin(0, 1); // Adjust the origin as needed
+//    } else {
+//        console.warn('Object is missing tile properties:', object);
+//    }
+// });
 
         this.player = new RoguePlayer(this, 10, 10, "rogue_player");
         this.physics.add.collider(this.player, this.ground);
